72363f9fe6df664d60d7be0bed486341ba90039d
TomaszGol <oplane2000@gmail.com>
2022-04-22 19:24:57 +0200

    Add structure and test

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,4 +1,45 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract Governance {}
+import "hardhat/console.sol";
+
+contract Governance {
+    // Voting model
+    struct Voting {
+        uint256 id;
+        string title;
+        uint256 minVotes;
+        uint256 rejectPercentageReq;
+        uint256 settlementPercentageReq;
+        uint256 startTimestamp;
+        uint256 endTimestamp;
+    }
+
+    mapping(uint256 => Voting) public votings;
+
+    uint256 public votingCounter;
+
+    mapping(address => bool) public voters;
+
+    constructor() {}
+
+    function addVoting(
+        string memory _title,
+        uint256 _minVotes,
+        uint256 _rejectPercentageReq,
+        uint256 _settlementPercentageReq,
+        uint256 _startTimestamp,
+        uint256 _endTimestamp
+    ) public {
+        votingCounter++;
+        votings[votingCounter] = Voting(
+            votingCounter,
+            _title,
+            _minVotes,
+            _rejectPercentageReq,
+            _settlementPercentageReq,
+            _startTimestamp,
+            _endTimestamp
+        );
+    }
+}
--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,19 @@
 import { expect } from "chai";
+//import { assert } from "console";
 import { ethers } from "hardhat";
 
-describe("Governance", function () {});
+describe("Governance", function () {
+  it("Voting should create and being writed to map", async function () {
+    const Governance = await ethers.getContractFactory("Governance");
+    const app = await Governance.deploy();
+    await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
+    // console.log(app.votings[1]);
+    const voting = await app.votings(1);
+    expect(voting.title).to.equal("Test1");
+    expect(voting.minVotes.toNumber()).to.equal(10);
+    expect(voting.rejectPercentageReq.toNumber()).to.equal(30);
+    expect(voting.settlementPercentageReq.toNumber()).to.equal(70);
+    expect(voting.startTimestamp.toNumber()).to.equal(1648569500);
+    expect(voting.endTimestamp.toNumber()).to.equal(1648589500);
+  });
+});
