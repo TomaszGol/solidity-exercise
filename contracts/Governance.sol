aabece8d5095a586f2282a4657a875e202b45ef1
TomaszGol <oplane2000@gmail.com>
2022-04-23 16:43:35 +0200

    Add vote func

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -8,7 +8,9 @@ contract Governance {
     struct Voting {
         uint256 id;
         string title;
-        uint256 voteCount;
+        uint256 forVoteCount;
+        uint256 againstVoteCount;
+        uint256 abstainVoteCount;
         uint256 minVotes;
         uint256 rejectPercentageReq;
         uint256 settlementPercentageReq;
@@ -36,6 +38,8 @@ contract Governance {
         votings[votingCounter] = Voting(
             votingCounter,
             _title,
+            0,
+            0,
             0,
             _minVotes,
             _rejectPercentageReq,
@@ -67,5 +71,25 @@ contract Governance {
 
     function setEndTimestamp(uint256 _id, uint256 _endTimestamp) public {
         votings[_id].endTimestamp = _endTimestamp;
+    }
+
+    function vote(uint256 _id) public {
+        require(!voters[msg.sender], "User already voted");
+        require(_id > 0 && _id <= votingCounter, "Voting dont exist");
+        voters[msg.sender] = true;
+        if (true) {
+            votings[_id].forVoteCount++;
+        } else if (false) {
+            votings[_id].againstVoteCount++;
+        } else {
+            votings[_id].abstainVoteCount++;
+        }
+    }
+
+    function countResult(uint256 _id) public view returns (uint256) {
+        uint256 sumOfVotes = votings[_id].forVoteCount +
+            votings[_id].againstVoteCount +
+            votings[_id].abstainVoteCount;
+        return (votings[_id].forVoteCount / sumOfVotes) * 100;
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -2,46 +2,83 @@ import { expect } from "chai";
 //import { assert } from "console";
 import { ethers } from "hardhat";
 
-describe("Governance", function () {
-  it("Voting should create and being writed to map", async function () {
-    const Governance = await ethers.getContractFactory("Governance");
-    const app = await Governance.deploy();
-    await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
-    // console.log(app.votings[1]);
-    const voting = await app.votings(1);
+describe("Token contract", function () {
+  let Governance;
+  let app;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-    expect(voting.title).to.equal("Test1");
-    expect(voting.voteCount).to.equal(0);
-    expect(voting.minVotes.toNumber()).to.equal(10);
-    expect(voting.rejectPercentageReq.toNumber()).to.equal(30);
-    expect(voting.settlementPercentageReq.toNumber()).to.equal(70);
-    expect(voting.startTimestamp.toNumber()).to.equal(1648569500);
-    expect(voting.endTimestamp.toNumber()).to.equal(1648589500);
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    const Governance = await ethers.getContractFactory("Governance");
+
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    app = await Governance.deploy();
   });
 
-  it("We should be able to change vars in voting ", async function () {
-    const Governance = await ethers.getContractFactory("Governance");
-    const app = await Governance.deploy();
-    await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
+  describe("Governance", function () {
+    it("Should create and being writed to map", async function () {
+      await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
+      // console.log(app.votings[1]);
+      const voting = await app.votings(1);
 
-    await app.setMinVotes(1, 20);
-    let voting = await app.votings(1);
-    expect(voting.minVotes.toNumber()).to.equal(20);
+      expect(voting.title).to.equal("Test1");
+      expect(voting.forVoteCount).to.equal(0);
+      expect(voting.againstVoteCount).to.equal(0);
+      expect(voting.abstainVoteCount).to.equal(0);
+      expect(voting.minVotes.toNumber()).to.equal(10);
+      expect(voting.rejectPercentageReq.toNumber()).to.equal(30);
+      expect(voting.settlementPercentageReq.toNumber()).to.equal(70);
+      expect(voting.startTimestamp.toNumber()).to.equal(1648569500);
+      expect(voting.endTimestamp.toNumber()).to.equal(1648589500);
+    });
 
-    await app.setRejectedPercReq(1, 20);
-    voting = await app.votings(1);
-    expect(voting.rejectPercentageReq.toNumber()).to.equal(20);
+    it("Should be able to change vars in voting", async function () {
+      await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
 
-    await app.setSettlementPercReq(1, 65);
-    voting = await app.votings(1);
-    expect(voting.settlementPercentageReq.toNumber()).to.equal(65);
+      const changeMinVotes = await app.setMinVotes(1, 20);
+      let voting = await app.votings(1);
+      expect(voting.minVotes.toNumber()).to.equal(20);
 
-    await app.setStarTimestamp(1, 1648569600);
-    voting = await app.votings(1);
-    expect(voting.startTimestamp.toNumber()).to.equal(1648569600);
+      await app.setRejectedPercReq(1, 20);
+      voting = await app.votings(1);
+      expect(voting.rejectPercentageReq.toNumber()).to.equal(20);
 
-    await app.setEndTimestamp(1, 1648589900);
-    voting = await app.votings(1);
-    expect(voting.endTimestamp.toNumber()).to.equal(1648589900);
+      await app.setSettlementPercReq(1, 65);
+      voting = await app.votings(1);
+      expect(voting.settlementPercentageReq.toNumber()).to.equal(65);
+
+      await app.setStarTimestamp(1, 1648569600);
+      voting = await app.votings(1);
+      expect(voting.startTimestamp.toNumber()).to.equal(1648569600);
+
+      await app.setEndTimestamp(1, 1648589900);
+      voting = await app.votings(1);
+      expect(voting.endTimestamp.toNumber()).to.equal(1648589900);
+    });
+
+    it("Should add vote", async function () {
+      await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
+      await app.vote(1);
+      let voting = await app.votings(1);
+
+      expect(voting.forVoteCount.toNumber()).to.equal(1);
+    });
+
+    it("Should return voting error", async function () {
+      await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
+      await app.vote(1);
+
+      await expect(app.vote(1)).to.be.revertedWith("User already voted");
+    });
+
+    it("Should return error with wrong id", async function () {
+      await app.addVoting("Test1", 10, 30, 70, 1648569500, 1648589500);
+
+      await expect(app.vote(99)).to.be.revertedWith("Voting dont exist");
+    });
   });
 });
